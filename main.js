461d5c875fde4040ee3ceb0dbf4ffc1881b5c791
Aicyraa <182109647+Aicyraa@users.noreply.github.com>
2025-12-18 08:35:35 +0800

    cleaner code

--- main.js
+++ main.js
@@ -10,39 +10,36 @@
    ISSUE
    1. How to change the number of grids but with the container size still the same
    2. How to make the color more darker when the mouse clicked on a square that already has a background color
-
+   3. Paano mag papalit ng bg color
+   4. paano mag ttrigger ung event listener habang naka click
 */
 
-let gridContainer = document.querySelector(".container-grids");
-const rangeBtn = document.getElementById("grid");
-let colorSetting = document.getElementById("color");
+const gridContainer = document.querySelector(".container-grids");
 
-rangeBtn.addEventListener("mouseup", (e) => {
-   // formula for creating dynamic grid
-   gridContainer.innerHTML = "";
-   let value = e.target.value;
-   let width = gridContainer.clientWidth;
-   let squareSize = width / value
-   let total = 0
-   document.getElementById("grid-current-value").textContent = value;
-
-   for (let squareCount = 0; squareCount < (value * value); squareCount++) {
+function setGrid(event) {
+   let squareGrid = event ? event.target.value : 16;
+   let containerWidth = gridContainer.clientWidth;
+   removeGrid();
+   for (let squares = 0; squares < squareGrid * squareGrid; squares++) {
+      let squareSize = containerWidth / squareGrid;
       let square = document.createElement("div");
       square.setAttribute("class", "grid-item");
-      square.style.width = ` ${squareSize}px `;
-      square.style.height = ` ${squareSize}px `;
-      gridContainer.appendChild(square);
-      total += 1
+      square.style.width = `${squareSize}px`;
+      square.style.height = `${squareSize}px`;
+      gridContainer.appendChild(square)
    }
+}
 
-   console.log(total);
-   
-   if (gridContainer.lastElementChild.clientWidth > squareSize) {
-      console.log(squareSize);
-      console.log(gridContainer.lastElementChild.clientWidth);
-      total - 1
-      gridContainer.lastElementChild.remove();
-   } 
-   console.log(total);
-   
-});
+function removeGrid() {
+   const currentGridItem = document.querySelectorAll(".grid-item");
+   currentGridItem.forEach((item) => {
+      item.remove();
+   });
+}
+
+(function () {
+   const rangeBtn = document.querySelector("#grid");
+   rangeBtn.addEventListener("input", setGrid);
+
+   setGrid();
+})();
